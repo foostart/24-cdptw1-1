8da0717c0c096a0180e06539ac1fb57a7a6500b2
xuanphi220820 <xuanphi08101999@gmail.com>
2019-09-24 13:05:34 +0700

    Delete

--- 1703/js/1673.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$("#type-1673-show-popup").css("display","none");
-	setTimeout(function () {
-    $("#type-1673-show-popup").show(); 
-  	}, 4000);
-  	$("#close-popup").click(function () {
-  		$("#type-1673-show-popup").css("display","none");
-  	});
-});
