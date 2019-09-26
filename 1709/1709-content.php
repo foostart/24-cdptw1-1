75d66ce445b64468fb07e11d65d9aea8596c4a3e
xuanphi220820 <xuanphi08101999@gmail.com>
2019-09-26 15:46:00 +0700

    fix

--- 1709/1709-content.php
+++ 1709/1709-content.php
@@ -22,7 +22,7 @@ $url_path = str_replace('\\', '/', $url_path);
 			</ul>
 			<div class="tab_container col-xs-12">
 				<div id="tab1" class="tab_content"> 
-					<div class="col-md-4 col-sm-12 col-xs-12 collection_img">
+					<div class="col-md-4 col-sm-12 col-xs-12">
 						<img class="img-responsive" src="http://<?php echo $url_path ?>/image/coll_desktops_medium.png" alt="Desktops">
 					</div>
 					<div class="col-md-8 col-sm-12 col-xs-12 collection_desc">
@@ -33,10 +33,10 @@ $url_path = str_replace('\\', '/', $url_path);
 					</div>
 				</div>
 				<div id="tab3" class="tab_content">
-					<div class="col-md-4 collection_img">
+					<div class="col-md-4 col-sm-12 col-xs-12">
 						<img class="img-responsive" src="http://<?php echo $url_path ?>/image/laptop.png" alt="Laptops">
 					</div>
-					<div class="col-md-8 collection_desc">
+					<div class="col-md-8 col-sm-12 col-xs-12 collection_desc">
 						<div class="rte">
 							<p>We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.So, it is natural that this sphere is one of the most popular and it is really hard to offer computer hardware because of great number of competitors. We are providing a great choice of different commodities. We are producing reliable and durable goods.</p>
 							<p>That is why we are always in touch with the latest new inventions and improvements. There is no doubt that we are the leaders and you don’t have to worry about our reputation because it is perfect.&nbsp; We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.</p>
@@ -44,10 +44,10 @@ $url_path = str_replace('\\', '/', $url_path);
 					</div>
 				</div>
 				<div id="tab4" class="tab_content">
-					<div class="col-md-4 collection_img">
+					<div class="col-md-4 col-sm-12 col-xs-12">
 						<img class="img-responsive" src="http://<?php echo $url_path ?>/image/phone.png" alt="Phones">
 					</div>
-					<div class="col-md-8 collection_desc">
+					<div class="col-md-8 col-sm-12 col-xs-12 collection_desc">
 						<div class="rte">
 							<p>We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.So, it is natural that this sphere is one of the most popular and it is really hard to offer computer hardware because of great number of competitors. We are providing a great choice of different commodities. We are producing reliable and durable goods.</p>
 							<p>That is why we are always in touch with the latest new inventions and improvements. There is no doubt that we are the leaders and you don’t have to worry about our reputation because it is perfect.&nbsp; We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.</p>
@@ -55,13 +55,13 @@ $url_path = str_replace('\\', '/', $url_path);
 					</div>
 				</div>
 				<div id="tab5" class="tab_content">
-					<div class="col-md-4 collection_img">
+					<div class="col-md-4 col-sm-12 col-xs-12">
 						<img class="img-responsive" src="http://<?php echo $url_path ?>/image/table.png" alt="Tablets">
 					</div>
-					<div class="col-md-8 collection_desc">
+					<div class="col-md-8 col-sm-12 col-xs-12 collection_desc">
 						<div class="rte">
 						   <p>We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.So, it is natural that this sphere is one of the most popular and it is really hard to offer computer hardware because of great number of competitors. We are providing a great choice of different commodities. We are producing reliable and durable goods.</p>
-							<p class="t2">That is why we are always in touch with the latest new inventions and improvements. There is no doubt that we are the leaders and you don’t have to worry about our reputation because it is perfect.&nbsp; We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.</p>
+							<p>That is why we are always in touch with the latest new inventions and improvements. There is no doubt that we are the leaders and you don’t have to worry about our reputation because it is perfect.&nbsp; We’re sure that no one could stay indifferent because everybody wants to get professionally made up product and pay a fair price for that.</p>
 						</div>
 					</div>
 				</div>
